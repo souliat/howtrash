5e8a08acb1bd9e39212c239a650fca0843264f82
souliat <souliat@naver.com>
2022-05-11 23:21:30 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -83,7 +83,7 @@ def check_dup():
     exists = bool(db.users.find_one({"username": username_receive}))
     return jsonify({'result': 'success', 'exists': exists})
 
-# 중분류 아이템들 다 보여주는 것.
+# 중분류 아이템들 다 가져와서 던져주는 것.
 @app.route("/get_posts", methods=['GET'])
 def get_posts():
     item_list = list(db.items.find({}, {'_id': False}))
@@ -141,13 +141,15 @@ def posting():
         user_info = db.users.find_one({"username": payload["id"]})
         comment_receive = request.form["comment_give"]
         date_receive = request.form["date_give"]
+        num_receive = request.form["num_give"]
 
         doc = {
             "username": user_info["username"],
             "profile_name": user_info["profile_name"],
             "profile_pic_real": user_info["profile_pic_real"],
             "comment": comment_receive,
-            "date": date_receive
+            "date": date_receive,
+            "item_num": num_receive
         }
 
         db.posts.insert_one(doc)
@@ -156,7 +158,7 @@ def posting():
         return redirect(url_for("home"))
 
 
-# 댓글 가져오기
+# 댓글 가져오기 + 상세페이지는 특정 댓글만 가져오기
 @app.route("/get_comments", methods=['GET'])
 def get_comments():
     token_receive = request.cookies.get('mytoken')
@@ -164,11 +166,12 @@ def get_comments():
         if token_receive is not None:
             payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
 
-            username_receive = request.args.get("username_give")
-            if username_receive == "":
+            num_receive = request.args.get("num_give") # 해당 상세페이지에 해당하는 num이 get방식으로 전달되었으므로 args.get 사용
+
+            if num_receive == "":
                 posts = list(db.posts.find({}).sort("date", -1).limit(20))
-            else: # 유저네임이 넘어왔다면 프로필 페이지에서 get_posts() 함수를 불렀다는 뜻.
-                posts = list(db.posts.find({"username": username_receive}).sort("date", -1).limit(20))
+            else: # num_receive가 있다면 detail.html에서 불렸다는 소리.
+                posts = list(db.posts.find({"item_num": num_receive}).sort("date", -1).limit(20))
 
             for post in posts:
                 post["_id"] = str(post["_id"]) # 좋아요를 눌렀는지 안눌렀는지를 확인할 고유 식별자를 id값으로 하며 이를 str타입으로 변경하여 사용.
